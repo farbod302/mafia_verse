7fc1b72574da2e2edf9563a07b1d2888c5a45a0e
farbod <f.aliakbari302@gmail.com>
2023-07-26 16:44:17 +0330

    main

--- routs/report.js
+++ routs/report.js
@@ -8,10 +8,11 @@ router.post("/new_report", async (req, res) => {
 
     const { token, user_reported, game_id, report_type } = req.body
     let user = Jwt.verify(token)
+    let now = Date.now()
     if (!user) return res.json({ status: false, msg: "invalid token" })
     let is_report_submitted = await Report.findOne({ game_id, user_submitted: user.uid })
     if (is_report_submitted) return res.json({ status: false, msg: "report submitted already" })
-    new Report({ report_type, user_reported, user_submitted: user.uid, game_id }).save()
+    new Report({ report_type, user_reported, user_submitted: user.uid, game_id, date: now }).save()
     res.json({ status: true, msg: "" })
     //check for report conditions
 
