33fb7851ac25d874a57e93a0b6e7d6dc76e7f3dc
farbod <f.aliakbari302@gmail.com>
2023-08-23 16:22:19 +0330

    main

--- socket/online_users_handler.js
+++ socket/online_users_handler.js
@@ -9,7 +9,7 @@ const online_users_handler = {
     add_user(user_id, socket_id) {
         let json_file = fs.readFileSync(`${__dirname}/online_users.json`)
         json_file = JSON.parse(json_file.toString())
-        json_file=json_file.filter(e=>e.user_id === user_id)
+        json_file=json_file.filter(e=>e.user_id !== user_id)
         json_file.push({ user_id, socket_id })
         fs.writeFileSync(`${__dirname}/online_users.json`, JSON.stringify(json_file))
     },
