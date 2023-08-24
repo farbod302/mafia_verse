fb88441fcff99bd78bd382341537c5d8f511175a
farbod <f.aliakbari302@gmail.com>
2023-08-24 16:45:25 +0330

    main

--- socket/find_match.js
+++ socket/find_match.js
@@ -94,7 +94,7 @@ const find_match = {
                 user_image: user_avatar
             }
         })
-        console.log({users});
+        console.log({mod_game_users:users});
         users = await Promise.all(users)
         users=users.filter(e => e.user_id !== creator)
         let game_id = uuid(4)
