8691cf46e43661824082f53307ca5f957774c093
farbod <f.aliakbari302@gmail.com>
2023-09-27 18:51:44 +0330

    main

--- routs/reanking.js
+++ routs/reanking.js
@@ -6,8 +6,9 @@ const router = express.Router()
 router.get("/", async (req, res) => {
     const ranking = await User.find({}).sort({ session_rank: 1 }).limit(50)
     const clean_ranking = ranking.map(user => {
-        const { idenity, session_rank, ranking, avatar, } = user
-        return { idenity, session_rank, ranking, avatar }
+        const { idenity, session_rank, ranking, avatar,points } = user
+        const {win,lose}=poin
+        return { idenity, session_rank, ranking, avatar,win,lose }
     })
     res.json({
         status: true,
