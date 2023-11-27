8fb8e3c046de2d9442a3198d6c2d67f311237963
farbod302 <f.aliakbari302@gmail.com>
2023-11-27 11:13:45 +0330

    s

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,6 @@ module.exports = {
       watch: ["server"],
       // Delay between restart
       watch_delay: 1000,
-      ignore_watch : ["node_modules", "client/img", "\\.git", "*.log","online_users.json"],
+      ignore_watch : ["node_modules", "client/img", "\\.git", "*.log","socket/online_users.json"],
     }]
   }
