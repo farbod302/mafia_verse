7684cd15b114e1217be3bce9171bcf7f936c0581
farbod302 <f.aliakbari302@gmail.com>
2023-11-27 11:33:15 +0330

    df

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     apps: [{
-      script: "index.js",
-      watch: ["server"],
-      // Delay between restart
-      watch_delay: 1000,
-      ignore_watch : ["node_modules", "client/img", "\\.git", "*.log","socket/online_users.json"],
+        name: "mafia",
+        script: "index.js",
+        // Delay between restart
+        watch_delay: 1000,
+        ignore_watch: ["socket/online_users.json"],
     }]
-  }
+}
