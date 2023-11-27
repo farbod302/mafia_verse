2b0be050e7e89764228dfd90c2ce0d63b8b49cef
farbod302 <f.aliakbari302@gmail.com>
2023-11-27 11:45:01 +0330

    sdf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,6 @@ module.exports = {
         // Delay between restart
         watch_delay: 1000,
         ignore_watch: ["node_modules","files","socket/online_users.json","socket/server_channel_msg/last_msgs.json"],
+        exec_mode:"cluster"
     }]
 }
