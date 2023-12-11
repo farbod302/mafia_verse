141deccbb6b40140bcf5a346ecf81d977a9082a4
farbod302 <f.aliakbari302@gmail.com>
2023-12-11 08:58:50 +0330

    df

--- games/tv/game.js
+++ games/tv/game.js
@@ -913,7 +913,7 @@ const Game = class {
 
         this.socket.to(game_id).emit("current_speech_end", { data: { user_id: queue[turn - 1]?.user_id } })
         await Helper.delay(1)
-
+        this.play_voice("1")
         let cur_speech = queue[turn]
         const cur_user_status = player_status.find(e => e.user_id === cur_speech.user_id)
         if (!cur_user_status.user_status.is_connected) {
