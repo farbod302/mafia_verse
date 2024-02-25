992bfee04045c1732d58dd3faec95fb5d37dd216
farbod302 <f.aliakbari302@gmail.com>
2024-02-25 18:54:14 +0330

    sdf

--- index.js
+++ index.js
@@ -46,13 +46,13 @@ app.use(middle)
 
 
 
-mongoose.connect(process.env.DB_SERVER)
+mongoose.connect(process.env.DB)
 let keys = Object.keys(imports)
 
 
 const conf = {
-    key: fs.readFileSync("/etc/letsencrypt/live/mafia.altf1.ir/privkey.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/live/mafia.altf1.ir/fullchain.pem")
+    key: fs.readFileSync("/etc/letsencrypt/live/mafia.gamingverse.ir/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/mafia.gamingverse.ir/fullchain.pem")
 }
 
 
