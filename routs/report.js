82ce6ea14698dc2d20ac5c5da55293f01416b703
farbod302 <f.aliakbari302@gmail.com>
2024-03-04 10:18:03 +0330

    add report

--- routs/report.js
+++ routs/report.js
@@ -4,16 +4,15 @@ const Report = require("../db/report")
 const router = express.Router()
 
 
-router.post("/new_report", async (req, res) => {
-
-    const { token, user_reported, game_id, report_type } = req.body
-    let user = Jwt.verify(token)
+router.post("/game_play", async (req, res) => {
+    const user = req.body.user
+    const { report_id, game_id, kind } = req.body
     let now = Date.now()
     if (!user) return res.json({ status: false, msg: "invalid token" })
     let is_report_submitted = await Report.findOne({ game_id, user_submitted: user.uid })
-    if (is_report_submitted) return res.json({ status: false, msg: "report submitted already" })
-    new Report({ report_type, user_reported, user_submitted: user.uid, game_id, date: now }).save()
-    res.json({ status: true, msg: "", data: {} })
+    if (is_report_submitted) return res.json({ status: false, msg: "گزارش شما قبلا ثبت شده" })
+    new Report({ kind, report_id, user_submitted: user.uid, game_id, date: now }).save()
+    res.json({ status: true, msg: "گزارش شما ثبت شد", data: {} })
     //check for report conditions
 
 })
